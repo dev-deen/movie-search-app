51589cdc8481353670b297c4bc6f4da8b02b40e9
fardeen-khan <80711481+dev-deen@users.noreply.github.com>
2025-01-08 21:10:18 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axiosInstance from './api/axiosInstance';
 import Navbar from './components/Navbar';
 import MovieCard from './components/MovieCard';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
 
   
-  const [searchTerm, setSerachTerm] = useState('');
+  const [searchTerm, setSerachTerm] = useState('Fast and Furious');
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasSearch, setHasSearch]= useState(false);
 
-  function search(searchItem){
+  //Load movies in start using useState
+  useEffect(() => {
+    search();
+  }, [page, hasSearch]);
+
+
+
+  function search(){
+
+    //search starts here
+    setLoading(true);
+
     const params = {
       'apikey': '737f3d5e',
-      's': searchItem
+      's': searchTerm,
+      'page': page
     }
 
     axiosInstance.get('', {params})
     .then((response)=>{
-      console.log(response);
       setData(response.data);
+      console.log(response);
+      setMovies((prevMovies)=>prevMovies.concat((response.data.Search ||[])));
       setLoading(false);
+
+      //search completed
+      // setHasSearch(false);
     })
     .catch((err) =>{
       console.log(err);
       setError(err.message);
       setLoading(false);
     });
+
   }
 
   const handleSubmit = (e) =>{
     e.preventDefault();
-    search(searchTerm);
+    setMovies([]);
+    setPage(1);
+    setHasSearch(!hasSearch);
+    // search();     
   }
 
   
@@ -43,14 +67,23 @@ function App() {
     <Navbar searchTerm={searchTerm} setSearchTerm={setSerachTerm} handleSubmit={handleSubmit}/>
     <div className="container my-4">
       {loading && <p>Loading....</p>}
-      {!loading && (error ? <p>Something went wrong: {error}</p> :
-        <div className="row">
-          {data && data.Search.map((movie, index)=>{
+      {!loading && error && <p>Something went wrong: {error}</p>}
+      {data?.totalResults == 0 ? <h2>No Results Found</h2>:
+      (
+      <InfiniteScroll className="row"
+      dataLength={movies?.length}
+      next={()=>{setPage(page+1)}}
+      hasMore={movies?.length < data?.totalResults}
+      loader={<h2>Loading......</h2>}
+      >
+          {movies.map((movie, index)=>{
             return <MovieCard movie={movie} key={index}/>
           })}
-        </div>
+      </InfiniteScroll>
       )}
     </div>
+
+      
     </div>
   );
 }
